cc6b2d8b8462bde5565d7803fd9bdca31d29f58c
Omar Tulashvili <omartulashvili.ge@gmail.com>
2025-12-20 21:05:00 +0300

    create const categories, list questions

--- main.go
+++ main.go
@@ -2,56 +2,66 @@ package main
 
 import "fmt"
 
+type Category string
+
+// категории
+const (
+	Biology    Category = "Биология"
+	Psychology Category = "Психология"
+)
+
 type Question struct {
 
 	// у вопроса есть принадлежность к одной из секций
 	// пока пусть вопрос будет принадлежать к какой-то одной секции, например, Биология
-	Section string
+	Category
 
 	// сам вопрос
-	Title string
+	Text string
+}
 
-	// у вопроса есть какой-то по 5-ти бальной шкале,
-	// который пользователь должен вводить самостоятельно через stdin
-	Answer int
+type Answer struct {
+	// ответ
+	Scale int
+}
+
+// список вопросов и категорий
+var QUESTIONS = []Question{
+	{
+		Category: Psychology,
+		Text:     "Насколько высокий уровень стресса сегодня? (1 - очень стрессовый день, 5 - стресса почти не было)",
+	},
+	{
+		Category: Psychology,
+		Text:     "Продвинулся ли ты по целям, которые ты себе наметил хотя бы на чуть-чуть?",
+	},
+	{
+		Category: Biology,
+		Text:     "Проснулся(-ась) в одно и то же время, чувствуя себя отдохнувшим? (1-5)",
+	},
+	{
+		Category: Biology,
+		Text:     "Избегал простых сахаров и обработанной еды? (1 - в питании было много сахара, 5 - сахара не было)",
+	},
+	{
+		Category: Biology,
+		Text:     "В рационе было достаточно белка и клетчатки? (1 - не было, 5 - да)",
+	},
 }
 
 func main() {
-	question := Question{
-		Section: "Биология",
-		Title:   "Проснулся(-ась) в одно и то же время, чувствуя себя отдохнувшим? (1-5)",
-		Answer:  0,
-	}
+	for _, questions := range QUESTIONS {
 
-	sections := []string{"Биология"}
+		fmt.Println("Категория:", questions.Category)
+		fmt.Println("Вопрос:", questions.Text)
 
-	biologyQuestions := map[int]string{
-		1: "Проснулся(-ась) в одно и то же время, чувствуя себя отдохнувшим? (1-5)",
-		2: "Избегал простых сахаров и обработанной еды? (1- в питании было много сахара, 5 - сахара, обработанной еды  вообще не было)",
-		3: "В рационе было достаточно белка и клетчатки? (1 - не было вообще, 5 - да)клетчатка содержится в фруктах, овощах, бобовых",
-	}
-
-	var answer int
-	var numQuestion int = 1
-	var numSection int = 0
-
-	for {
-		question.Section = sections[numSection]
-		question.Title = biologyQuestions[numQuestion]
-
-		fmt.Printf("Секция: %s\nВопрос: %s\n", question.Section, question.Title)
-		fmt.Printf("Ответ:  ")
-		fmt.Scanln(&answer)
-		question.Answer = answer
-		fmt.Println("----------------")
-
-		if numQuestion == len(biologyQuestions) {
-			break
-		} else {
-			numQuestion++
-			continue
+		// для каждого вопроса мы "создаем" свой ответ
+		var initAnswer = Answer{
+			Scale: 0,
 		}
+		fmt.Scanln(&initAnswer.Scale)
+		fmt.Println("Ответ:", initAnswer.Scale)
+		fmt.Println("--------------------")
 
 	}
-
 }
