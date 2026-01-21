dc598485592f7317e2f032da53668e9593c04d05
Omar Tulashvili <omartulashvili.ge@gmail.com>
2026-01-21 21:22:57 +0300

    start cobra intergrity

--- cmd/main.go
+++ cmd/main.go
@@ -4,9 +4,12 @@ import (
 	"time"
 
 	app "github.com/tulashvili/MyDailyControlQuestions/internal"
+	"github.com/tulashvili/MyDailyControlQuestions/internal/ui/cli"
 )
 
 func main() {
 	time.Local = time.UTC
 	app.NewApp()
+
+	cli.Execute()
 }
